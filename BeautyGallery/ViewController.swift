a6132c440452b4a327c47a1a831abbba24a775a7
Jake Lin <jakelinau@gmail.com>
2014-08-29 23:00:48 +1000

    Initial Commit

---
+++ BeautyGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyGallery
+//
+//  Created by Jake Lin on 29/08/2014.
+//  Copyright (c) 2014 rushjet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
